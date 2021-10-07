eef3d2095a0e7336ab89f96e07e23428b38bd43d
Ale <akip-core@outlook.com>
2021-10-07 15:34:46 +0200

    light done

--- app/src/main/java/com/example/lampadinaex2g9/MainActivity.kt
+++ app/src/main/java/com/example/lampadinaex2g9/MainActivity.kt
@@ -2,12 +2,31 @@ package com.example.lampadinaex2g9
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
+import android.widget.RadioButton
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        
+        var on:RadioButton = findViewById(R.id.on)
+        var off:RadioButton = findViewById(R.id.off)
+        var lightOn:ImageView = findViewById(R.id.l_on)
+        var lightOff:ImageView = findViewById(R.id.l_off)
+
+        on.setOnClickListener {
+            lightOff.visibility = View.INVISIBLE
+            lightOn.visibility = View.VISIBLE
+        }
+
+        off.setOnClickListener {
+            lightOn.visibility = View.INVISIBLE
+            lightOff.visibility = View.VISIBLE
+        }
+
+
+
     }
 }
